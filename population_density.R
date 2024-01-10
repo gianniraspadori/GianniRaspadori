2a222c719f8b572742fc9aa87682e476de5098c7
gianniraspadori <116293797+gianniraspadori@users.noreply.github.com>
2024-01-10 03:19:38 +0100

    Create population_density.R

---
+++ population_density.R
@@ -0,0 +1,68 @@
+# A package is needed for point pattern analysis
+#if it is the first time we use a package we need to install it
+install.packages("spatstat")
+
+#then recall it with the library function
+library(spatstat)
+
+# let's use the bei data:
+bei
+
+# plotting the data
+plot(bei)
+
+# changing dimension - cex
+plot(bei, cex=.2)
+
+# changing the symbol - pch
+plot(bei, cex=.2, pch=19)
+
+# additional datasets
+bei.extra
+plot(bei.extra)
+
+# let's use only part of the dataset: elev
+plot(bei.extra$elev)
+#then rename the part used 
+elevation <- bei.extra$elev
+plot(elevation)
+
+# second method to select elements
+elevation2 <- bei.extra[[1]]
+plot(elevation2)
+
+# passing from points to a countinuous surface
+densitymap <- density(bei)
+plot(densitymap)
+points(bei, cex=.2)
+#we rename the new outcome and change some specifications 
+
+#thanks to colorRampPalette we can change all the colours in densitymap but we first assign the new color to the element cl and the we plot 
+cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(100)
+plot(densitymap, col=cl)
+
+cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(4)
+plot(densitymap, col=cl)
+
+clnew <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)
+plot(densitymap, col=clnew)
+
+plot(bei.extra)
+
+elev <- bei.extra[[1]] # bei.extra$elev
+plot(elev)
+
+# multiframe
+#we put together the two images in one , the first number in the graph stands for the number of rows the second the columns
+par(mfrow=c(1,2))
+plot(densitymap)
+plot(elev)
+
+par(mfrow=c(2,1))
+plot(densitymap)
+plot(elev)
+
+par(mfrow=c(1,3))
+plot(bei)
+plot(densitymap)
+plot(elev)
