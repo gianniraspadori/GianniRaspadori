f25621a6df6c93a930918a47bba2d7c0bcff5ac9
gianniraspadori <116293797+gianniraspadori@users.noreply.github.com>
2024-01-13 16:23:14 +0100

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -4,7 +4,7 @@
 
 library(terra)
 library(ncdf4)
-
+library(raster)
 ## I downloaded some images I will work with from Copernicus' dataset , now I need to set the directory so I can import those images on R
 
 setwd(C:/Users/User/OneDrive/Desktop)
@@ -18,3 +18,35 @@ fcover18<-rast("")
 plot(fcover18)
 fcover23<-rast("")
 plot(fcover23)
+
+##now I have too many images for each dataset to work with, so I will take just the first image for each dataset
+## They show the Leaf Area Index (LAI) during a decade (2014,2018,2023)
+
+lai14<-fcover14[[1]]
+plot(lai14)
+
+lai18<-fcover18[[1]]
+plot(lai18)
+
+lai23<-fcover23[[1]]
+plot(lai23)
+
+## those are images of the entire world , but I want to focus my project on the Amazon region
+## So I need to the coordinates to border just the area I will use
+
+lat_min=15000
+lat_max=23000
+lon_min=35000
+lon_max=45000
+
+## Then by using the "extent" functions I can  group all these value
+
+square<-extent(lon_min,lon_max,lat_min,lat_max)
+
+## now I screen all the parts I do not need , obtaining just the images of the Amazon for each year
+
+amazon14<-crop(lai14, Square)
+amazon18<-crop(lai18, Square)
+amazon23<-crop(lai23, Square)
+
+## 
